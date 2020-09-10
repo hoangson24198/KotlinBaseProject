77e8b06ddf5a3cc4e9f50c1c2a42f42a1ac941cf
Lê Trần Hoàng Sơn <hoangson24198@gmail.com>
2020-09-10 17:36:12 +0700

    first commit

---
+++ app/src/main/java/com/example/firebaseStarterKit/model/SlideContent.kt
@@ -0,0 +1,5 @@
+package com.example.firebaseStarterKit.model
+
+import android.graphics.drawable.Drawable
+
+data class SlideContent(val image: Drawable, val header: String, val description: String)
